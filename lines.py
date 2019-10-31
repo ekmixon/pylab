31dc13546ddbf0ab5c74ce3bcf0daabbd97a95bb
Andy Lester <andy@petdance.com>
2019-10-30 20:18:44 -0500

    Matches against a pattern

--- lines.py
+++ lines.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
+import re
 
 filepath = '/Users/andy/range.txt'
-with open( filepath, 'r' ) as fh:
+regex = re.compile('fa')
+with open(filepath, 'r') as fh:
     n = 0
     line = fh.readline()
     while line:
         n += 1
-        print( line )
+        line = line.rstrip()
+        match = re.search(regex, line)
+        if match:
+            print(line)
         line = fh.readline()
     fh.close()
