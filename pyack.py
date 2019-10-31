003df56434be772652612dbf32cbed60aa5c4bee
Andy Lester <andy@petdance.com>
2019-10-30 20:59:17 -0500

    Adding exclusion of swap files

--- pyack.py
+++ pyack.py
@@ -5,7 +5,8 @@ import sys
 import re
 
 def findhits(filepath, regex):
-    print('looking in ' + filepath)
+    #print('looking in ' + filepath)
+    any_printed = False
     with open(filepath, 'r') as fh:
         n = 0
         line = fh.readline()
@@ -14,17 +15,22 @@ def findhits(filepath, regex):
             line = line.rstrip()
             match = re.search(regex, line)
             if match:
-                print(line)
+                if not any_printed:
+                    print(filepath)
+                    any_printed = True
+                print(str(n) + ': ' + line)
             line = fh.readline()
         fh.close()
 
 # Set the directory you want to start from
-rootDir = sys.argv[1]
+text_regex = sys.argv[1]
+rootDir = sys.argv[2]
 gitpath = rootDir + '/.git'
+regex = re.compile(text_regex)
 
-regex = re.compile('f9')
 
-
+swapfile_regex = re.compile('\.swp$')
+tilde_regex = re.compile('~$')
 for dirpath, dirnames, filenames in os.walk(rootDir):
     if dirpath == gitpath:
         dirnames[:] = []
@@ -32,5 +38,9 @@ for dirpath, dirnames, filenames in os.walk(rootDir):
     dirnames.sort()
     filenames.sort()
     for fname in filenames:
-        findhits(dirpath + fname, regex)
+        if re.search(swapfile_regex, fname):
+            continue
+        if re.search(tilde_regex, fname):
+            continue
+        findhits(dirpath + '/' + fname, regex)
 
