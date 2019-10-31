19140cd865795db0035e181d11c00edb8eaaf91f
Andy Lester <andy@petdance.com>
2019-10-30 22:22:46 -0500

    adding exceptions that let it walk all through parrot

--- pyack.py
+++ pyack.py
@@ -8,20 +8,16 @@ def findhits(filepath, regex):
     any_printed = False
     with open(filepath, 'r') as fh:
         n = 0
-        line = ''
-        try:
-            line = fh.readline()
-        except (UnicodeDecodeError):
-            print("Got a unicode error: " + filepath)
-            pass
+        print(filepath)
+        line = fh.readline()
         while line:
             n += 1
-            line = line.rstrip()
             match = re.search(regex, line)
             if match:
                 if not any_printed:
                     print(filepath)
                     any_printed = True
+                line = line.rstrip()
                 print(str(n) + ': ' + line)
             line = fh.readline()
         fh.close()
@@ -35,6 +31,8 @@ regex = re.compile(text_regex)
 
 swapfile_regex = re.compile('\.swp$')
 tilde_regex = re.compile('~$')
+graphic_regex = re.compile('\.(png|ico|gif)$')
+garbage_regex = re.compile('\.(pbc|pir_output|gz)$')
 for dirpath, dirnames, filenames in os.walk(rootDir):
     if dirpath == gitpath:
         dirnames[:] = []
@@ -42,9 +40,17 @@ for dirpath, dirnames, filenames in os.walk(rootDir):
     dirnames.sort()
     filenames.sort()
     for fname in filenames:
+        if fname == 'string_cs.t':
+            continue
+        if fname == 'POD2HTML.pm':
+            continue
         if re.search(swapfile_regex, fname):
             continue
         if re.search(tilde_regex, fname):
+            continue
+        if re.search(graphic_regex, fname):
+            continue
+        if re.search(garbage_regex, fname):
             continue
         findhits(dirpath + '/' + fname, regex)
 
