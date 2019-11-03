ce833c4eb8eaa1c4826baadaff01acfe56dc1493
Andy Lester <andy@petdance.com>
2019-11-02 20:56:38 -0500

    experimenting with three threads

---
+++ pyack3.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+# pyack.py but with threads.
+
+import os
+import sys
+import re
+
+from queue import Queue
+import threading
+
+search_queue = Queue()
+filter_queue = Queue()
+
+def findhits():
+    while 1:
+        filepath, regex = search_queue.get()
+        if filepath is None:
+            return
+
+        any_printed = False
+        with open(filepath, 'r', encoding='ISO8859') as fh:
+            n = 0
+            line = fh.readline()
+            while line:
+                n += 1
+                match = regex.search(line)
+                if match:
+                    if not any_printed:
+                        print(filepath)
+                        any_printed = True
+                    line = line.rstrip()
+                    print(str(n) + ': ' + line)
+                line = fh.readline()
+            fh.close()
+
+def filefilter():
+    swapfile_regex = re.compile('\\.swp$')
+    tilde_regex = re.compile('~$')
+    graphic_regex = re.compile('\\.(png|ico|gif)$')
+    garbage_regex = re.compile('\\.(pbc|gz)$')
+
+    while 1:
+        filepath, regex = filter_queue.get()
+        if filepath is None:
+            return
+        if fname == 'string_cs.t':
+            continue
+        if fname == 'POD2HTML.pm':
+            continue
+        if swapfile_regex.search(fname):
+            continue
+        if tilde_regex.search(fname):
+            continue
+        if graphic_regex.search(fname):
+            continue
+        if garbage_regex.search(fname):
+            continue
+        # Put our file in the queue, rather than calling findhits
+        search_queue.put([filepath, regex])
+
+search_thread = threading.Thread(target=findhits)
+search_thread.start()
+
+filter_thread = threading.Thread(target=filefilter)
+filter_thread.start()
+
+# Set the directory you want to start from
+text_regex = sys.argv[1]
+rootDir = sys.argv[2]
+gitpath = rootDir + '/.git'
+regex = re.compile(text_regex)
+
+for dirpath, dirnames, filenames in os.walk(rootDir):
+    if dirpath == gitpath:
+        dirnames[:] = []
+        continue
+    dirnames.sort()
+    filenames.sort()
+    for fname in filenames:
+        filter_queue.put([dirpath + '/' + fname, regex])
+
+# Tell the findhits to stop
+search_queue.put([None,None])
+filter_queue.put([None,None])
+search_thread.join()
+filter_thread.join()
